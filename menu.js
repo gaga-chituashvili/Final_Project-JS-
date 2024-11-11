ebfbf4822b3e5fdf87a6aa3ae8eb0e744040a897
gaga-chituashvili <gagachituashvili7@gmail.com>
2024-11-11 17:12:50 +0400

    final_project_js

---
+++ menu.js
@@ -0,0 +1,22 @@
+
+
+const left = document.querySelector('.left');
+const right = document.querySelector('.right');
+const sliderItem = document.querySelectorAll('.slider_item');
+let activeIndex = 0;
+
+function updateActiveItem() {
+  sliderItem.forEach(item => item.classList.remove('slider_item_active'));
+  sliderItem[activeIndex].classList.add('slider_item_active');
+}
+
+
+left.addEventListener('click', () => {
+  activeIndex = (activeIndex > 0) ? activeIndex - 1 : sliderItem.length - 1;
+  updateActiveItem();
+});
+
+right.addEventListener('click', () => {
+  activeIndex = (activeIndex < sliderItem.length - 1) ? activeIndex + 1 : 0;
+  updateActiveItem();
+});
