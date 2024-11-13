6860f6b5968c6bfde5b2f2277ca94e5228372e1f
gaga-chituashvili <gagachituashvili7@gmail.com>
2024-11-14 02:20:24 +0400

    Final_Project_JS

--- menu.js
+++ menu.js
@@ -1,4 +1,26 @@
 
+
+//burger icon//
+
+const burger = document.querySelector('.header_burger');
+const xmark = document.querySelector('.header_xmark');
+const headerNav = document.querySelector('.header_nav');
+
+function toggleMenu() {
+  const isMenuVisible = headerNav.classList.contains('show');
+
+  headerNav.classList.toggle('show', !isMenuVisible);
+  burger.style.display = isMenuVisible ? 'block' : 'none';
+  xmark.style.display = isMenuVisible ? 'none' : 'block';
+}
+
+burger.addEventListener('click', toggleMenu);
+
+xmark.addEventListener('click', toggleMenu);
+
+
+
+
 //slider//
 
 const left = document.querySelector('.left');
@@ -23,6 +45,21 @@ right.addEventListener('click', () => {
 });
 
 
+
+
+const header = document.querySelector('.header');
+
+
+function handleScroll() {
+    if (window.scrollY > 50) {
+        header.classList.add('transparent-header');
+    } else {
+        header.classList.remove('transparent-header');
+    }
+}
+
+// Listen to the scroll event
+window.addEventListener('scroll', handleScroll);
 
 
 
--- script.js
+++ script.js
@@ -1,4 +1,41 @@
 //header//
+const header = document.querySelector('.header');
+
+function handleScroll() {
+    if (window.scrollY > 50) {
+        header.classList.add('transparent-header');
+    } else {
+        header.classList.remove('transparent-header');
+    }
+}
+
+window.addEventListener('scroll', handleScroll);
+
+
+//burger icon//
+
+const burger = document.querySelector('.header_burger');
+const xmark = document.querySelector('.header_xmark');
+const headerNav = document.querySelector('.header_nav');
+
+function toggleMenu() {
+  const isMenuVisible = headerNav.classList.contains('show');
+
+  headerNav.classList.toggle('show', !isMenuVisible);
+  burger.style.display = isMenuVisible ? 'block' : 'none';
+  xmark.style.display = isMenuVisible ? 'none' : 'block';
+}
+
+burger.addEventListener('click', toggleMenu);
+
+xmark.addEventListener('click', toggleMenu);
+
+
+
+
+
+
+//menus//
 
 const fetchProduct = async () => {
   try {
@@ -115,4 +152,6 @@ inputs.forEach((input) => {
 textarea.addEventListener("keyup", (event) => {
   validateInput(event.target);
 });
+
+
 
